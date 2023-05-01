45a78ecf257b764e043590b8e6c9ec3e0e9fc696
Nikhitha5545 <nikithalakshman@gmail.com>
2023-05-01 20:04:42 +0530

    first commit

---
+++ model/model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema({
+    name:String,
+    brand:String,
+ 
+});
+
+module.exports = mongoose.model("Product",dataSchema);
